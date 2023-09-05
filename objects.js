28a71cf11233c855910ee43809119b5f653af1b4
Victoriabunmi72 <bhummhie97@gmail.com>
2023-09-05 09:47:31 +0100

    objects.js

---
+++ objects.js
@@ -0,0 +1,30 @@
+// Embeded Object.
+// Encapsulating all objects...(encapsulating means grouping data together).
+
+width: 1400;
+height: 600;
+color: "space gray";
+keyboardSize: "13inch";
+trackPadType: "guesture";
+typeText: function(){...};
+smash: function(){...};
+
+camera: {
+            quality: "HD"
+            record: function(){...}
+        }
+screen: {
+            type: "retina";
+            size: "13inch";
+            backlight: "LED"
+        }
+keyboard:{
+            gesture: true;
+            material: "glass";
+            pinch: function(){...}
+        }
+
+// This code include all about object:
+// *Nouns, Adjectives _properties.
+// *Verbs _functions.
+// *Hierarchy _object within object.
